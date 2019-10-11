edcfaac56a541c718c8a64962dcc033cd7d321b4
L200170115 <43403869+L200170115@users.noreply.github.com>
2019-10-11 10:04:40 +0700

    Add files via upload

---
+++ Modul_5/delete.php
@@ -0,0 +1,22 @@
+<?php 
+    $conn = mysqli_connect('localhost', 'root', '', 'informatika');
+    // mengambil data dari URL
+    $nim = $_GET['nim'];
+    // menghapus data
+    $hapus = "DELETE FROM mahasiswa WHERE NIM='$nim'";
+    $data = mysqli_query($conn, $hapus);
+    // jika ada row yang terpengaruh, lebih dari 0 maka data terhapus
+    if ($data>0) {
+        echo "
+            <script>
+                alert('Data Berhasil Dihapus!');
+                document.location.href='form_tugas.php';
+            </script>";
+    } else {
+        echo "
+            <script>
+                alert('Data Gagal Dihapus!');
+                document.location.href='form_tugas.php';
+            </script>";
+    }
+?>
