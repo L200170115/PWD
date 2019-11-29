b0f838ef2532a1b512563a0cc608723c656d64bb
L200170115 <43403869+L200170115@users.noreply.github.com>
2019-11-29 09:28:53 +0700

    Add files via upload

---
+++ Modul_9/tugas/member.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Welcome member</title>
+    <link rel="stylesheet" href="member.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><h1>Selamat Datang Member</h1>
+logout klik link <a href="logout2.php" class="link"><u>Disini</u></a></center>
+</body>
+</html>
